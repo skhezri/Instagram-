68c5a4c0ff2d4640bd94a7edf108bd5dc927c603
Sophia Khezri <skhezri@fb.com>
2018-07-09 15:07:20 -0700

    Initial Commit

---
+++ Instagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Instagram
+//
+//  Created by Sophia Khezri on 7/9/18.
+//  Copyright © 2018 Sophia Khezri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
